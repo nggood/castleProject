fa3c466000d15f9bb0bedec63bd2f33884f70b86
Nicolas Goudemant <ngoudemant@gmail.com>
2018-02-16 02:42:45 +0100

    Initial release

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const fetch = require('fetch');
+
+const BASE_URL = 'http://castles.poulpi.fr/castles/1/rooms';
+const init = 'http://castles.poulpi.fr/castles/1/rooms/entry';
+const monHistorique = { };
+const successChest = [ ];
+
+const checkRooms = async (urlTab, roomsResponse = []) => {
+
+};
+
